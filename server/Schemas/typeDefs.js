550381e9b10901d3957f4466a9f134dc6768c9e8
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-12 15:59:47 -0400

    typDefs Schema set up

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [], description: String!, title: String!, bookId: ID!, image: img!): User
+    removeBook(bookId: ID!): User
+}
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID
+    authors:[String]
+    description: String
+    title: String
+    : img
+    link: link
+}
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const server = new ApolloServer({
+  typeDEfs,
+  resolvers,
+  context: authMiddleware,
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,6 +23,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 });
+}
+
+startApolloServer(typeDefs, resolvers);
