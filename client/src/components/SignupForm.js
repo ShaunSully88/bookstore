41bbe2311f22a455bc91327792d949434b0d7bd5
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-17 15:27:15 -0400

    trying to dbeug

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from  '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
-  // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
-  const [validated] = useState(false);
-  // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
+    const [userFormData, setUserFormData] = useState ({
+      username:"", 
+      email:"", 
+      password:"", 
+    });
+    const [validated] = useState(false);
+    const[showAlert, setShowAlert] = useState(false);
 
-  const [addUser, { error }] = useMutation(ADD_USER);
+    const [addUser, { error }] = useMutation(ADD_USER);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
+    
   };
 
   const handleFormSubmit = async (event) => {
@@ -48,6 +52,7 @@ const SignupForm = () => {
     });
   };
 
+      
   return (
     <>
       {/* This is needed for the validation functionality above */}
