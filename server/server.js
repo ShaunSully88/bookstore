6a52a1e18aaee2198f92e1638b9b3d56645523b7
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-18 10:08:31 -0400

    debugging

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-const { typeDefs, resolvers } = require('./Schemas')
-const db = require('./config/connection');
-const routes = require('./routes');
-const { authMiddleware } = require('./utils/auth');
 
-const app = express();
+const { typeDefs, resolvers } = require('./Schemas')
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-app.use(express.urlencoded({ extended: true }));
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const routes = require('./routes');
+
+
 
 // if we're in production, serve client/build as static assets
 //if (process.env.NODE_ENV === 'production') {
