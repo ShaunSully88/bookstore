aff01e3781c170578ff92662d84de87440c09596
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-18 10:51:03 -0400

    updated mongodb

--- server/server.js
+++ server/server.js
@@ -39,6 +39,6 @@ db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 });
-}
+};
 
 startApolloServer(typeDefs, resolvers);
